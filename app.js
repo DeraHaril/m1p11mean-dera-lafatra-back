0307d7c96a70933258d7a86b6b11e8000b45495b
Dera <deraharilala@gmail.com>
2024-02-13 20:00:02 +0300

    Ajout de Connexion et Inscription de l'user
    Connexion/inscription, avec utilisation de bcrypt pour le hashage de password et jwt pour la gestion de token

--- app.js
+++ app.js
@@ -4,39 +4,53 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const uri = process.env.MONGODB_URI || 'mongodb+srv://Dera:Danzerdt1%40%40@cluster0.wpsqo.mongodb.net/';
+MongoClient.connect(uri, { useUnifiedTopology: true
+})
+.then(client => {
+  console.log('Connected to Database');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  const db = client.db('salon_beaute_mean');
+  const userCollection = db.collection('user');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+    app.use('/', indexRouter);
+    app.use('/users', usersRouter(userCollection));
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+      next(createError(404));
+    });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // error handler
+    app.use(function(err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
+  })
+  .catch(error => console.error(error))
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,86 @@
+const { json } = require('body-parser');
 var express = require('express');
 var router = express.Router();
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.TOKEN_KEY || 'clé_secrète_par_défaut';
+const bcrypt = require('bcrypt');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = (userCollection) => {
+  const userController = require('../controllers/userController')(userCollection);
+  /* GET users listing. */
+  
+  router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+  });
 
-module.exports = router;
+  router.post('/login', traitementConnexionUser = async(req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    try{
+      const user = await userController.getUserByEmail(email);
+      if(user){
+        console.log(user.password);
+        console.log(password);
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if(passwordMatch){
+          const token = jwt.sign({ userId: user._id, role: user.role}, secretKey, { expiresIn: '1h' });
+          res.json({ success: true, message: 'Connexion réussie', token });
+        }
+        else{
+          res.json({ success: false, message: "mot de passe incorrecte"});
+        }
+      } 
+      else{
+        res.json({ success: false, message: "cet email est invalide"});
+    }
+    } catch(error){
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
+  const hashPassword = async (password) => {
+    try {
+        const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+
+        const hashedPassword = await bcrypt.hash(password, salt);
+        return hashedPassword;
+    } catch (error) {
+        console.error(error);
+        throw new Error('Erreur lors du hachage du mot de passe');
+    }
+  };
+
+  router.post('/inscription', traitementInscription = async(req,res) =>{
+    const nom = req.body.nom;
+    const prenom = req.body.prenom;
+    const email = req.body.email;
+    const password = req.body.password;
+    const password_verification = req.body.password_verification;
+    const role = req.body.role;
+
+    if(password === password_verification){
+      try{
+        const hashedPassword = await hashPassword(password);
+        const user = await userController.createUser(nom,prenom,email, hashedPassword, role); 
+
+        //Connexion de l'user apres inscription
+        const userInscrit = await userController.getUserByEmail(email);
+        const token = jwt.sign({ userId: userInscrit._id, userInscrit: userInscrit.role}, secretKey, { expiresIn: '1h' });
+        res.json({ success: true, message: 'Inscription terminée avec succès', token });
+      } catch(error){
+        console.error(error);
+        throw new Error("Erreur de traitement d'inscription");
+      }
+    }
+    else{
+      res.json({ success: false, message: 'mot de passe et vérification du mot de passe non identique' })
+    }
+  });
+
+  return router;
+}
+
+
+
